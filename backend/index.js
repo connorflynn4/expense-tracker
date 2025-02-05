8c430da1c2392ee965a06986aadef307ca363445
Connor Flynn <connorflynn4@gmail.com>
2025-02-04 22:02:02 -0800

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+import dotenv from "dotenv";
+
+import { ApolloServer } from "@apollo/server"
+
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+
+
+import mergedResolvers from "./resolvers/index.js"
+import mergedTypeDefs from "./typeDefs/index.js"
+
+dotenv.config();
+
+const app = express()
+ 
+const httpServer = http.createServer(app)
+
+ 
+const server = new ApolloServer({
+  typeDefs: mergedTypeDefs,
+  resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+})
+ 
+ await server.start();
+
+ app.use(
+   '/',
+   cors(),
+   bodyParser.json(),
+   // expressMiddleware is a function that takes a server and returns a middleware function
+   // an apollo server instance and optional configuration options
+   expressMiddleware(server, {
+     context: async ({ req }) => ({ req }),
+   })
+ );
+
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000`);
+   
