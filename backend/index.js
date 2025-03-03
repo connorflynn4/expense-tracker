61be7d21d3454033cded1ee9a2d8a81dd1c09bf5
Connor Flynn <connorflynn4@gmail.com>
2025-03-02 18:26:18 -0800

    passport configuration

--- backend/index.js
+++ backend/index.js
@@ -5,20 +5,53 @@ import bodyParser from "body-parser";
 
 import dotenv from "dotenv";
 
+import passport from "passport";
+import session from "express-session";
+import connectMongo from "connect-mongodb-session";
+
 import { ApolloServer } from "@apollo/server"
 
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 
+import { buildContext } from "graphql-passport";
 
 import mergedResolvers from "./resolvers/index.js"
 import mergedTypeDefs from "./typeDefs/index.js"
 
+import { configurePassport } from "./passport/passport.config.js";
+import { connectDB } from "./db/connectDB.js";
+
+
+
 dotenv.config();
+configurePassport();
 
 const app = express()
  
 const httpServer = http.createServer(app)
+
+const MongoDBStore = connectMongo(session);
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+  collection: "sessions",
+});
+
+store.on("error", (err) => console.log(err));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false, // this option specifies whether to save the session to the store on every request
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week,
+    httpOnly: true, //this option prevents Cross-Site Scripting (XSS) attacks
+  },
+  store: store,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
  
 const server = new ApolloServer({
@@ -31,15 +64,19 @@ const server = new ApolloServer({
 
  app.use(
    '/',
-   cors(),
+   cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+   }),
    bodyParser.json(),
    // expressMiddleware is a function that takes a server and returns a middleware function
    // an apollo server instance and optional configuration options
    expressMiddleware(server, {
-     context: async ({ req }) => ({ req }),
+     context: async ({ req, res }) => buildContext({ req,res }),
    })
  );
 
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+await connectDB();
 console.log(`🚀 Server ready at http://localhost:4000`);
    
